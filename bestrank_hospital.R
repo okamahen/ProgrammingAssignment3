ac1214541d4258ba675fe9b7bfbe1d25625fc855
Oka Mahendra Saputra <okamahen@alumni.itb.ac.id>
2020-11-19 01:18:03 +0700

    Add filter for State, filter successfull, follow up on validation

--- bestrank_hospital.R
+++ bestrank_hospital.R
@@ -17,6 +17,9 @@ best <- function(state, outcome) {
   ## Read outcome data
   data <- new_outcome
   ## Check that state and outcome are valid
+  data %>% filter(
+    state %in% state %>% view()
+  )
   ## Return hospital name in that state with lowest 30-day death
   ## rate
 }
