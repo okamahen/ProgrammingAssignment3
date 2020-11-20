99c28f39e5795571ac9543e49e5473011b90eda4
Oka Mahendra Saputra <52365870+okamahen@users.noreply.github.com>
2020-11-20 16:09:08 +0700

    Update best.R

--- best.R
+++ best.R
@@ -39,11 +39,3 @@ best <- function(state, outcome) {
   view(fin)
   
 }
-
-
-rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
-  ## Check that state and outcome are valid
-  ## Return hospital name in that state with the given rank
-  ## 30-day death rate
-}
